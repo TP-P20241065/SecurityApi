72dbf815e847ee61d842569014cd26864f6081f6
Rojoxt <delamarzp@gmail.com>
2024-06-30 15:37:29 -0500

    fest: add report controller

--- report_management/controller/report_controller.py
+++ report_management/controller/report_controller.py
@@ -2,47 +2,47 @@ from typing import Optional, Dict, Any
 
 from fastapi import APIRouter, Path, Body, Query
 from shared.response.schema import ResponseSchema
-from unit_management.model.unit import CreateUnitModel
-from unit_management.service.unit_service import UnitService
+from report_management.model.report import CreateReportModel
+from report_management.service.report_service import ReportService
 
 router = APIRouter(
-    prefix="/unit",
-    tags=["unit"], )
+    prefix="/report",
+    tags=["report"], )
 
 
 @router.get("", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_all_unit():
-    result = await UnitService.get_all()
+async def get_all_report():
+    result = await ReportService.get_all()
     return result
 
 
 # Ruta para obtener un driver por ID
 @router.get("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_unit_by_id(id: int):
-    result = await UnitService.get_by_id(id)
+async def get_report_by_id(id: int):
+    result = await ReportService.get_by_id(id)
     return result
 
 
 # Ruta para filtrar drivers
 @router.get("/", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_filtered_units(name: Optional[str]):
-    result = await UnitService.get_filtered(name)
+async def get_filtered_reports(name: Optional[str]):
+    result = await ReportService.get_filtered(name)
     return result
 
 
 @router.put("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
-async def update_unit(driver_id: int = Path(..., alias="id"), *, updateUnit: CreateUnitModel):
-    result = await UnitService.update(driver_id, updateUnit)
+async def update_report(camera_id: int = Path(..., alias="id"), *, updateCamera: CreateReportModel):
+    result = await ReportService.update(camera_id, updateCamera)
     return result
 
 
 @router.delete("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
-async def delete_unit(unit_id: int = Path(..., alias="id")):
-    result = await UnitService.delete_by_id(unit_id)
+async def delete_report(camera_id: int = Path(..., alias="id")):
+    result = await ReportService.delete_by_id(camera_id)
     return result
 
 
 @router.post("", response_model=ResponseSchema, response_model_exclude_none=True)
-async def create_unit(create_data: CreateUnitModel):
-    result = await UnitService.create(create_data)
+async def create_report(create_data: CreateReportModel):
+    result = await ReportService.create(create_data)
     return result
--- unit_management/controller/camera_controller.py
+++ unit_management/controller/camera_controller.py
@@ -11,38 +11,38 @@ router = APIRouter(
 
 
 @router.get("", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_all_unit():
+async def get_all_camera():
     result = await CameraService.get_all()
     return result
 
 
 # Ruta para obtener un driver por ID
 @router.get("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_unit_by_id(id: int):
+async def get_camera_by_id(id: int):
     result = await CameraService.get_by_id(id)
     return result
 
 
 # Ruta para filtrar drivers
 @router.get("/", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_filtered_units(name: Optional[str]):
+async def get_filtered_cameras(name: Optional[str]):
     result = await CameraService.get_filtered(name)
     return result
 
 
 @router.put("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
-async def update_unit(camera_id: int = Path(..., alias="id"), *, updateCamera: CreateCameraModel):
+async def update_camera(camera_id: int = Path(..., alias="id"), *, updateCamera: CreateCameraModel):
     result = await CameraService.update(camera_id, updateCamera)
     return result
 
 
 @router.delete("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
-async def delete_unit(camera_id: int = Path(..., alias="id")):
+async def delete_camera(camera_id: int = Path(..., alias="id")):
     result = await CameraService.delete_by_id(camera_id)
     return result
 
 
 @router.post("", response_model=ResponseSchema, response_model_exclude_none=True)
-async def create_driver(create_data: CreateCameraModel):
+async def create_camera(create_data: CreateCameraModel):
     result = await CameraService.create(create_data)
     return result
