243793ad3a06cf5b551281f295e7519c6e7e26e3
Rojoxt <delamarzp@gmail.com>
2024-06-30 14:38:04 -0500

    fear: add camera controller

---
+++ unit_management/controller/camera_controller.py
@@ -0,0 +1,48 @@
+from typing import Optional, Dict, Any
+
+from fastapi import APIRouter, Path, Body, Query
+from shared.response.schema import ResponseSchema
+from unit_management.model.camera import CreateCameraModel
+from unit_management.service.camera_service import CameraService
+
+router = APIRouter(
+    prefix="/camera",
+    tags=["camera"], )
+
+
+@router.get("", response_model=ResponseSchema, response_model_exclude_none=True)
+async def get_all_unit():
+    result = await CameraService.get_all()
+    return result
+
+
+# Ruta para obtener un driver por ID
+@router.get("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
+async def get_unit_by_id(id: int):
+    result = await CameraService.get_by_id(id)
+    return result
+
+
+# Ruta para filtrar drivers
+@router.get("/", response_model=ResponseSchema, response_model_exclude_none=True)
+async def get_filtered_units(name: Optional[str]):
+    result = await CameraService.get_filtered(name)
+    return result
+
+
+@router.put("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
+async def update_unit(camera_id: int = Path(..., alias="id"), *, updateCamera: CreateCameraModel):
+    result = await CameraService.update(camera_id, updateCamera)
+    return result
+
+
+@router.delete("/{id}", response_model=ResponseSchema, response_model_exclude_none=True)
+async def delete_unit(camera_id: int = Path(..., alias="id")):
+    result = await CameraService.delete_by_id(camera_id)
+    return result
+
+
+@router.post("", response_model=ResponseSchema, response_model_exclude_none=True)
+async def create_driver(create_data: CreateCameraModel):
+    result = await CameraService.create(create_data)
+    return result
