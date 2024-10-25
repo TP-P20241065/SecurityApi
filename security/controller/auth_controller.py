dbaa7b8487b19a8559be5b84ad501debce522c84
rojoxt <delamarzp@gmail.com>
2024-10-24 23:53:15 -0500

    fix: fixed auth portl

--- security/controller/auth_controller.py
+++ security/controller/auth_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path, Body, Query, HTTPException
+from fastapi import APIRouter, Path, Body, Query, HTTPException, Depends
 
 from security.model.auth import Register, DataToken, TokenModel
 from security.service.auth_service import AuthService
@@ -15,27 +15,30 @@ async def register(register: Register):
     return ResponseSchema(detail="hashed created!", result=result)
 
 
-@router.post("/login", response_model=TokenModel)
-async def login(login: Register):
+@router.post("/login/portal-desktop", response_model=TokenModel)
+async def login_portal_desktop(login: Register):
     # Verificar si el usuario existe
     if await AuthService.user_existed(login.email):
         user = await AuthService.get_user(login.email)
         # Verificar si la cuenta está activa
         if user.isActive:
-            # Verificar si la contraseña es correcta
-            if AuthService.verify_password(login.password, user.hashedPassword):
-                token = await AuthService.create_access_token(user)
-                return token
+            if 2 in user.permissions:
+                if AuthService.verify_password(login.password, user.hashedPassword):
+                    token = await AuthService.create_access_token(user)
+                    return token
+                else:
+                    # Contraseña incorrecta
+                    raise HTTPException(status_code=400, detail="Password or email incorrect")
             else:
-                # Contraseña incorrecta
-                raise HTTPException(status_code=400, detail="Password or email incorrect")
+                # Usuario no tiene permiso para acceder al aplicacion de escritorio
+                raise HTTPException(status_code=400,
+                                    detail="No tienes permiso para acceder la aplicación de escritorio.")
         else:
             # Usuario inactivo
             raise HTTPException(status_code=400, detail="The account is deactivated")
     else:
         # Correo electrónico no registrado
         raise HTTPException(status_code=400, detail="Email not registered")
-
 
 
 @router.patch("/reset-password", response_model=ResponseSchema)
@@ -45,3 +48,31 @@ async def reset_password(email: str):
         return result
     else:
         raise HTTPException(status_code=400, detail="Email not existed")
+
+
+@router.post("/login/portal-admin", response_model=TokenModel)
+async def login_portal_admin(login: Register):
+    # Verificar si el usuario existe
+    if await AuthService.user_existed(login.email):
+        user = await AuthService.get_user(login.email)
+        # Verificar si la cuenta está activa
+        if user.isActive:
+            if 0 in user.permissions or 1 in user.permissions:
+                if AuthService.verify_password(login.password, user.hashedPassword):
+                    token = await AuthService.create_access_token(user)
+                    return token
+                else:
+                    # Contraseña incorrecta
+                    raise HTTPException(status_code=400, detail="Password or email incorrect")
+            else:
+                # Usuario no tiene permiso para acceder al portal administrativo
+                raise HTTPException(status_code=400,
+                                    detail="No tienes permiso para acceder al portal administrativo.")
+            # Verificar si la contraseña es correcta
+
+        else:
+            # Usuario inactivo
+            raise HTTPException(status_code=400, detail="The account is deactivated")
+    else:
+        # Correo electrónico no registrado
+        raise HTTPException(status_code=400, detail="Email not registered")
